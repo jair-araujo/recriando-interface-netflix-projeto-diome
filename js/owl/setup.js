1df9adefa247cfb963577ee4f2db0c4a138601bf
Jair Araujo <jair.jay@hotmail.com>
2021-11-30 23:23:42 -0300

    Projeto DIOME Clone NetFlix

---
+++ js/owl/setup.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        700:{
+            items:3
+        },
+        1000:{
+            items:4
+        },
+        1200:{
+            items:6
+        }
+    
+    }
+})
